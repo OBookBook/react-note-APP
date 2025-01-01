02bba689f1c4ef70cd1f637fdb7a31120561e2a4
N.I <iworksWeb@gmail.com>
2025-01-01 22:47:22 +0900

    add Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,18 @@
 import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea placeholder="write note"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">Title</h1>
+        <div className="markdown-preview">article</div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
