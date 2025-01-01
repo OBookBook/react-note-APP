24358c51748f1bb8b6aad1f81cc899b723123f54
N.I <iworksWeb@gmail.com>
2025-01-01 21:50:32 +0900

    att  type type Note
    "型 '{ id: number; title: string; content: string; modDate: number; }'
    を型 'never' に割り当てることはできません。",

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import { useState } from "react";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+  modDate: number;
+};
+
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState<Note[]>([]);
 
   const onAddNote = () => {
     const newNote = {
