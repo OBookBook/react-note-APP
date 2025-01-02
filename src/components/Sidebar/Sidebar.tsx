fcdc0c6a61fd6a00df45988a32b01b2010a64f41
N.I <iworksWeb@gmail.com>
2025-01-02 18:45:48 +0900

    add sort

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -16,6 +16,8 @@ const Sidebar = ({
   activeNote,
   setActiveNote,
 }: SidebarProps) => {
+  const sortedNotes = notes.sort((a, b) => b.modDate - a.modDate);
+
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -23,7 +25,7 @@ const Sidebar = ({
         <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
-        {notes.map((note) => (
+        {sortedNotes.map((note) => (
           <div
             className={`app-sidebar-note ${note.id === activeNote && "active"}`}
             key={note.id}
