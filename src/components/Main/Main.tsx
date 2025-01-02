d86caf973c98f61dd07b8949ab66517a489c05c9
N.I <iworksWeb@gmail.com>
2025-01-02 18:34:31 +0900

    add note edit

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,18 +3,38 @@ import { Note } from "../../types/Note";
 
 type MainProps = {
   activeNote: Note | undefined;
+  onUpdateNote: (updatedNote: Note) => void;
 };
 
-const Main = ({ activeNote }: MainProps) => {
+const Main = ({ activeNote, onUpdateNote }: MainProps) => {
+  const onEditNote = (key: keyof Note, value: string) => {
+    if (!activeNote) return;
+    onUpdateNote({
+      ...activeNote,
+      [key]: value,
+      modDate: Date.now(),
+    });
+  };
+
   if (!activeNote) {
-    return <div className="no-active-note">Node is not selected</div>;
+    return <div className="no-active-note">Note is not selected</div>;
   }
 
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
-        <input type="text" />
-        <textarea placeholder="write note"></textarea>
+        <input
+          id="title"
+          type="text"
+          value={activeNote.title}
+          onChange={(e) => onEditNote("title", e.target.value)}
+        />
+        <textarea
+          id="content"
+          placeholder="Write note"
+          value={activeNote.content}
+          onChange={(e) => onEditNote("content", e.target.value)}
+        ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
